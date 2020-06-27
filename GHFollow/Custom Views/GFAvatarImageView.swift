54680f0cfb89a9facefb53f9b40845a6c2f930a0
Saul Rivera <saulriveal@gmail.com>
2020-06-27 17:20:51 -0500

    Creation of custom collection view cell
    For user presentation

---
+++ GHFollow/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,29 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollow
+//
+//  Created by Saul Rivera on 27/06/20.
+//  Copyright © 2020 Saul Rivera. All rights reserved.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    let placeholderImage = UIImage(named: "avatar-placeholder")
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
