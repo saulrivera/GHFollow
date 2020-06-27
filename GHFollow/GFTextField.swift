e3ac54af011336218640412a1d88dae39748468f
Saul Rivera <saulriveal@gmail.com>
2020-06-27 00:59:40 -0500

    Custom views added

--- GHFollow/GFTextField.swift
+++ GHFollow/GFTextField.swift
@@ -12,6 +12,7 @@ class GFTextField: UITextField {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.configure()
     }
     
     required init?(coder: NSCoder) {
